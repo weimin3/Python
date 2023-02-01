971cec30419efc88f0f7d52b09cd4c53be150499
weimin3 <bonjour_world@126.com>
2023-02-01 14:33:09 +0100

    while

--- A-basic/Demo/Exercise1.py
+++ A-basic/Demo/Exercise1.py
@@ -16,6 +16,5 @@ stock_code = "00100"
 stock_price_daily_growth_factor = 1.2
 growth_days = 7
 print("公司：%s , 股票代码： %s , 当前股价： %.2f, 每日增长系数是：%.1f, 经过 %s 天增长后，股价达到了：%.2f " %(name, stock_code,stock_price,stock_price_daily_growth_factor,growth_days,(stock_price * (stock_price_daily_growth_factor ** growth_days))))
-
 print(f"公司：{name}, 股票代码：{stock_code} , 当前股价：{stock_price}, 每日增长系数是：{stock_price_daily_growth_factor}, 经过 {growth_days} 天增长后，股价达到了：{(stock_price * (stock_price_daily_growth_factor ** growth_days))} " )
 
